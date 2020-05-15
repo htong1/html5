dabb10ee9a4d9f53cae23b59465aa1e167115e9f
htong1 <htong0001@gmail.com>
2020-05-15 17:43:13 +0000

    Sequence grows every time the button i pressed.

--- math/fibonacci.js
+++ math/fibonacci.js
@@ -4,5 +4,14 @@ nextNum(){
   let answer = "" + this.sequence[this.sequence.length-1] + " + " + this.sequence[this.sequence.length-2] + " = " + 
 (this.sequence[this.sequence.length-1] + this.sequence[this.sequence.length-2])
 return answer;
+},
+next(){
+return (this.sequence[this.sequence.length-1] + this.sequence[this.sequence.length-2]);
+},
+
+grow(){
+this.sequence.push(this.next()) 
 }
+
 }
+
